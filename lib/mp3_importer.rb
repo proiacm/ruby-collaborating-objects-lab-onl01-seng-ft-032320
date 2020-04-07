e0ae9ec195b5cbc61fa147a8a4567903962b3f1e
CiaraMaria Proia <proiacm@gmail.com>
2020-04-07 00:54:36 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,5 +2,15 @@ class Mp3Importer
   attr_accessor :path 
   
   def initialize
+    @path = path
   end
+  
+  def files 
+    music_array - []
+    
+    Dir.new(self.path).each do |file| music_array << file 
+  end
+  
+  def import(list_of_filenames)
+    list_of_filenames.each {|some_filename| Song.new_by_filename(some_filename)}
 end 
