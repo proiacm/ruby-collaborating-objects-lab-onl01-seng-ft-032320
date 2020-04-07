51bbaa7162ace33f5630dcb7185c0d163631d757
CiaraMaria Proia <proiacm@gmail.com>
2020-04-07 01:29:29 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -14,6 +14,6 @@ class MP3Importer
     end
 
   def import
-    list_of_filenames.each {|some_filename| Song.new_by_filename(some_filename)}
+    files.each {|some_filename| Song.new_by_filename(some_filename)}
   end
 end 
