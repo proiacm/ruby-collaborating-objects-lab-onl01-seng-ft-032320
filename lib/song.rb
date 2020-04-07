3fef7e08f7631657bbc2f01a9c3a361c7f6d833d
CiaraMaria Proia <proiacm@gmail.com>
2020-04-07 01:40:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,8 +13,7 @@ class Song
   
   def self.new_by_filename(filename)
     song = self.new(filename.split(" - ")[1])
-    song.artist = filename.split(" - ")[0] 
-    song
+    self.artist = filename.split(" - ")[0]
   end
   
 
